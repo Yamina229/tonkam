d9160a47b57ee5ad5b5ed0c5dd55757b66cbed4d
Dorra <yamina.mansouri19@outlook.fr>
2024-10-23 23:42:29 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,11 +3,13 @@
 # Add Mozilla Team PPA for Firefox
 sudo add-apt-repository -y ppa:mozillateam/ppa
 
+sudo apt -y --fix-broken install
+
 # Update package list
 sudo apt update
-sudo apt install firefox
+
 # Install Firefox and required packages
-sudo apt install -y xdotool curl jq xclip unzip git python3-dev python3-tk python3-pip gnome-screenshot python3.8-venv zip 
+sudo apt install -y firefox xdotool zip curl jq xclip unzip git python3-dev python3-tk python3-pip gnome-screenshot python3.8-venv
 
 # Create Python virtual environment
 python3 -m venv myenv
@@ -22,6 +24,6 @@ touch ~/.Xauthority
 pip install pyautogui
 pip install --upgrade pillow
 pip install opencv-python-headless
-pip install pyperclip
+
 # Output completion message
 echo "All packages installed and environment set up successfully."
